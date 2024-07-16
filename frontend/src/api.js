bbff84663448bc4c685d55d9ac07b406a15bfa47
kzm/fhm <khuzaimanazik4@gmail.com>
2024-07-16 06:28:21 -0700

    change database

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,25 +1,3 @@
-// import axios from "axios";
-// import { ACCESS_TOKEN } from "./constants";
-
-// const apiURL = "/choreo-apis/djangoreactauth/backend/v1";
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
-// });
-
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem(ACCESS_TOKEN);
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
@@ -41,14 +19,36 @@ api.interceptors.request.use(
   }
 );
 
-export const loginUser = async (credentials) => {
-  try {
-    const response = await api.post("/login/", credentials);
-    return response.data;
-  } catch (error) {
-    console.error("Login error:", error);
-    throw error;
-  }
-};
-
 export default api;
+// import axios from "axios";
+// import { ACCESS_TOKEN } from "./constants";
+
+// const apiURL = "/choreo-apis/djangoreactauth/backend/v1";
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem(ACCESS_TOKEN);
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export const loginUser = async (credentials) => {
+//   try {
+//     const response = await api.post("/login/", credentials);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Login error:", error);
+//     throw error;
+//   }
+// };
+
+// export default api;
